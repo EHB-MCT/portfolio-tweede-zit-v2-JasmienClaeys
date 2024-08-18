b41b1c14510edab30172dbbd192b5975715af657
JasmienClaeys <jasmien.claeys@student.ehb.be>
2024-08-18 23:08:17 +0200

    feat: update app.js

--- build/images/api/src/app.js
+++ build/images/api/src/app.js
@@ -1,22 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const express = require("express");
+const cors = require("cors");
 const knex = require("knex");
 const knexfile = require("./db/knexfile");
 
+const app = express();
+
 const db = knex(knexfile.development);
 
-
-
-
-// Middleware setup
-app.use(cors());
 app.use(express.json());
 
-// Define routes here
-app.get('/', (req, res) => res.send('Hello World'));
-
+app.use(cors());
 
 module.exports = app;
-
-// db.raw("SELECT 1+1").then(() => console.log("done"));
